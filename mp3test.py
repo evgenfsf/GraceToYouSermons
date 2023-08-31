1d8f89026d9a66b9ec46bb14726db7ac4907ed31
Eugene Bondarenco <evgenfsf@protonmail.com>
2023-08-31 17:46:28 -0400

    use of requests.Session()

--- mp3test.py
+++ mp3test.py
@@ -1,13 +1,15 @@
 import requests
 from selenium import webdriver
 from bs4 import BeautifulSoup
+import re
+from loguru import logger
 
 options = webdriver.FirefoxOptions()
 options.add_argument('--headless')
 driver = webdriver.Firefox(options=options)
 driver.get("https://www.gty.org/library/resources/sermons-library")
 soup = BeautifulSoup(driver.page_source, features="html.parser")
-driver.quit()
+driver.quit()           #avoids resource waisting
 
 download_server = "https://cdn.gty.org/sermons/High/"
 
@@ -19,12 +21,14 @@ code = sermon_box.find(class_="code").span.string
 filename = f'{scripture}'
 link = f'{download_server}{code}.mp3'
 
-sermon = requests.get(link)
+session = requests.Session()
+sermon = session.get(link)
 
-with open(f'{code}.mp3', 'wb') as f:
-        f.write(sermon.content)
+if sermon.status_code == 200:                           #only save file if file properly received
+        logger.success(f"Download Succeeded: {filename}")
+        with open(f'{code}.mp3', 'wb') as f:            #open in binary mode to avoid errors
+                f.write(sermon.content)
+else:   logger.error(f"Download Failed: {filename}")
 
-pass
 
-#TODO requests.Session()
-#TODO stream=True
+#TODO stream=True ?
