25380485712e0007dd7f7459c62217b488de6016
evgenfsf <evgenfsf@gmail.com>
2023-09-28 00:08:19 -0400

    Correct errors

--- sermon_downloader.py
+++ sermon_downloader.py
@@ -1,6 +1,7 @@
 import requests
 from selenium import webdriver
 from bs4 import BeautifulSoup
+from bs4.element import Tag
 from loguru import logger
 import sys
 import os
@@ -40,9 +41,13 @@ class SermonDownloader():
         sermons = self.parser.findAll(class_="gty-asset store-library sermon")
         pagination_source = self.driver.find_element(By.CLASS_NAME, "col.s12.m7").find_element(By.CLASS_NAME, "pagination")
         pagination = self.GTYParser(pagination_source.get_attribute("outerHTML"))
-        pg_n = pagination.find(class_="active").a.string
-        last_chevron = pagination.find("i", class_="mdi-navigation-chevron-right").parent.parent
-        return Page(sermons, pg_n, last_chevron)
+        try:
+            pg_n = pagination.find(class_="active").a.string
+            last_chevron = pagination.find("i", class_="mdi-navigation-chevron-right").parent.parent
+        except AttributeError:  #just one page
+            pg_n = 1
+            last_chevron = True
+        return Page(sermons, int(pg_n), last_chevron)
     
     def return_book_dict(self):
         selector = self.driver.find_element(By.CLASS_NAME, "col.s8.l5").find_element(By.TAG_NAME, "select")
@@ -54,9 +59,9 @@ class SermonDownloader():
         bk_n = self.book_dict[name]
         ch_n = 0 #0 means all chapters
         pg_n = pg
-        logger.info(f"Creating folder: {name}")
-        os.mkdir(name)
-        os.chdir(name)
+        logger.info(f"Creating folder: {bk_n}_{name}")
+        os.mkdir(f"{bk_n}_{name}")
+        os.chdir(f"{bk_n}_{name}")
         while True:
             logger.info(f"Page {pg_n}")
             full_url=f"{self.baseurl}/{pg_n}?book={bk_n}&chapter={ch_n}"
@@ -87,6 +92,7 @@ class SermonDownloader():
         for book in self.book_dict.keys():
             self.download_book(book)
         os.chdir(os.pardir)
+        self.quit()
         
     
     def quit(self):
@@ -118,7 +124,10 @@ class Page():
                     s.download(session)
     
     def is_last(self):
-        return True if self.ch["class"][0] == "disabled" else False
+        if type(self.ch) == Tag:
+            return True if self.ch["class"][0] == "disabled" else False
+        else: 
+            return self.ch
 
 
 class Sermon(): 
@@ -142,14 +151,15 @@ class Sermon():
         
     def filename_correct(self):
         fn = f"{self.scripture}_{self.title}_{self.code}"
-        fn = fn.translate({ord(c): "-" for c in "!@#$%^&*()[]{};:,./<>?\|`~-=_+"})
+        fn = fn.translate({ord(c): "-" for c in "!@#$%^&*()[]{};:,./<>?\|`~-=_+\""})
         return f'{fn}.mp3'
     
     def download(self, session):
-        logger.info(f"Downloading {self.filename}")
-        download = session.get(f"{self.download_server}{self.code}.mp3")
+        link = f"{self.download_server}{self.code}.mp3"
+        logger.info(f"Downloading {link}")
+        download = session.get(link)
         if download.status_code == 200:
-            logger.success(f"Download Succeeded: {self.filename}")
+            logger.success(f"Download Succeeded: {link}")
             with open(self.filename, 'wb') as f:            #open in binary mode to avoid errors
                     f.write(download.content)
             logger.success(f"File Created: {self.filename}")
